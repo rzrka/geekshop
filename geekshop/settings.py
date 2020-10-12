ccd121045d71ed8ac016d49fbd4dc1dea004dbce
rzrka <rzrka1998@gmail.com>
2020-10-12 15:51:32 +0300

    Lesson 4. Data from files

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j%x+_6uu=4fxrw)787az2&readh^@36la*-k#atw7&!1-saglf'
+SECRET_KEY = 'pzegz3ade%pvc00#@7bxcz9mt8tv(^l!5a9iks0t_@)ra#&om$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,5 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
+
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,13 +1,18 @@
-
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 import mainapp.views as mainapp
 
-urlpatterns =[
+urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", mainapp.main),
-    path("products/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("products/", include("mainapp.urls", namespace="products")),
+    path("contact/", mainapp.contact, name="contact"),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
