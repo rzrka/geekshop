4f611bf03d36c16a1eaac365e56fa2db376dcd73
daniil1 <daniil1@daniil.Dlink>
2020-11-21 23:39:30 +0300

    lesson_7. View cache

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,6 +5,7 @@ from django.core.cache import cache
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from django.views.decorators.cache import cache_page
 
 from .models import Contact, Product, ProductCategory
 
@@ -166,3 +167,11 @@ def contact(request):
     locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
+
+@cache_page(600)
+def contact(request):
+    title = "о нас"
+    visit_date = timezone.now()
+    locations = Contact.objects.all()
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)    
