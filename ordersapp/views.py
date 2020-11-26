bd573ce7644aecca4b581fba2aeab198eb873f43
daniil <daniil1@daniil.Dlink>
2020-11-26 15:07:19 +0300

    lesson_8. F objects

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -13,6 +13,8 @@ from basketapp.models import Basket
 from mainapp.models import Product
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
+from django.db.models import F
+
 
 
 class OrderList(LoginRequiredMixin, ListView):
@@ -131,26 +133,16 @@ def order_forming_complete(request, pk):
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(instance, sender, **kwargs):
     if instance.pk:
-        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        instance.product.quantity = F("quantity") - (instance.quantity - sender.get_item(instance.pk).quantity)
     else:
-        instance.product.quantity -= instance.quantity
+        instance.product.quantity = F("quantity") - instance.quantity
     instance.product.save()
-    # quantity_total = instance.product.reserved + instance.product.quantity
-    # quantity_delta = quantity_total - instance.quantity
-    # if quantity_delta < 0:
-    #     instance.product.reserved = quantity_total
-    #     instance.quantity = instance.product.reserved
-    # else:
-    #     instance.product.reserved = instance.quantity
-    #     instance.product.quantity = quantity_delta
-    # instance.product.save()
 
 
 @receiver(pre_delete, sender=OrderItem)
 @receiver(pre_delete, sender=Basket)
 def product_quantity_update_delete(instance, **kwargs):
-    instance.product.quantity += instance.product.reserved
-    instance.product.reserved = 0
+    instance.product.quantity = F("quantity") + instance.quantity
     instance.product.save()
 
 
